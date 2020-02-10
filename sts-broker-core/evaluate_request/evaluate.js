91bf18844b9a2354c91a029e9acd31c3c7874524
Nascimento <nascit@88e9fe62b489.ant.amazon.com>
2020-02-10 17:29:30 +0000

    added step functions

---
+++ sts-broker-core/evaluate_request/evaluate.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const aws = require('aws-sdk');
+
+exports.lambdaHandler = async (event, context, callback) => {
+
+    // TODO: Sample business logic to decide whether this permission request should be automatically approved or not.
+
+    console.log(event);
+
+    if (event.team == "myteam") {
+        console.log("Manual approval needed");
+        var approved = false;
+    } else {
+        console.log("Permission request is approved");
+        var approved = true;
+    }
+
+    const response = {
+        automated_approval: approved
+    };
+
+    return response;
+};
