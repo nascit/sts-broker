293fced65498add5190ad806de6893f8b8791d11
nascit <timoteo.asn@gmail.com>
2020-03-11 17:01:37 +0000

    error handling for permission request

--- sts-broker-core/request_credentials/app.js
+++ sts-broker-core/request_credentials/app.js
@@ -29,91 +29,94 @@ exports.lambdaHandler = async(event, context, callback) => {
         return;
     }
 
-    var inline_policy = JSON.parse(event.body).inline_policy;
-    var broker_policy = JSON.parse(event.body).policy;
-    var sessionDuration = JSON.parse(event.body).sessionDuration;
+    try {
+        var inline_policy = JSON.parse(event.body).inline_policy;
+        var broker_policy = JSON.parse(event.body).policy;
+        var sessionDuration = JSON.parse(event.body).sessionDuration;
 
-    // TODO: Validate inline policy if not empty.
+        // TODO: Validate inline policy if not empty.
 
-    // TODO: Check if passed STS broker policy belong to the team allowed policies.
+        // TODO: Check if passed STS broker policy belong to the team allowed policies.
 
-    var userid = event.requestContext.authorizer.claims.sub;
-    inline_policy = (!inline_policy) ? "" : inline_policy["Statement"];
+        var userid = event.requestContext.authorizer.claims.sub;
+        inline_policy = (!inline_policy) ? "" : inline_policy["Statement"];
 
-    console.log("User has requested the following policy: ");
-    console.log(broker_policy);
+        console.log("User has requested the following policy: ");
+        console.log(broker_policy);
 
-    // Validate tags: Check if user has the attributes requited on user_tags
+        // Validate tags: Check if user has the attributes requited on user_tags
 
-    var params = {
-        TableName: process.env.POLICIES_TABLE,
-        Key: { policy_id: broker_policy },
-    };
-    const data = await docClient.get(params).promise();
-    const user_tags = data.Item.user_tags;
-    const default_tags = data.Item.default_tags;
-        
-    var tags = [];
+        var params = {
+            TableName: process.env.POLICIES_TABLE,
+            Key: { policy_id: broker_policy },
+        };
+        const data = await docClient.get(params).promise();
+        const user_tags = data.Item.user_tags;
+        const default_tags = data.Item.default_tags;
 
-    if (user_tags && user_tags.length != 0) {
-        user_tags.forEach(function(tag_name) {
-            var custom_tag = 'custom:' + tag_name;
-            var tag = {};
-            if (event.requestContext.authorizer.claims[custom_tag]) {
-                tag[tag_name] = event.requestContext.authorizer.claims[custom_tag];
-            }
-            else if (event.requestContext.authorizer.claims.tag) {
-                tag[tag_name] = event.requestContext.authorizer.claims.tag;
-            }
-            else {
-                errorResponse("This user does not have the attribute '" + tag + "' which is required to use this policy.", context.awsRequestId, callback);
-            }
-            tags.push(tag);
-        });
+        var tags = [];
+
+        if (user_tags && user_tags.length != 0) {
+            user_tags.forEach(function(tag_name) {
+                var custom_tag = 'custom:' + tag_name;
+                var tag = {};
+                if (event.requestContext.authorizer.claims[custom_tag]) {
+                    tag[tag_name] = event.requestContext.authorizer.claims[custom_tag];
+                }
+                else if (event.requestContext.authorizer.claims.tag) {
+                    tag[tag_name] = event.requestContext.authorizer.claims.tag;
+                }
+                else {
+                    throw new Error("This user does not have the attribute '" + tag_name + "' which is required to use this policy.");
+                }
+                tags.push(tag);
+            });
+        }
+
+        if (default_tags && default_tags.length != 0) {
+            default_tags.forEach(function(tag) {
+                tags.push(tag);
+            });
+        }
+
+        var item = {
+            requestid: uuid.v1(),
+            userid: userid,
+            email: event.requestContext.authorizer.claims.email,
+            team: event.requestContext.authorizer.claims['custom:team'],
+            inline_policy: inline_policy,
+            policy: broker_policy,
+            tags: tags,
+            sessionDuration: sessionDuration,
+            timestamp: new Date().getTime()
+        };
+
+        var params = {
+            TableName: process.env.REQUESTS_TABLE,
+            Item: removeEmptyStringElements(item),
+        };
+
+        await docClient.put(params).promise();
+
+        const response = {
+            statusCode: 200,
+            body: "Permission request made by user '" + userid + "' (" + event.requestContext.authorizer.claims.email + ") . Please allow some time until the security admin evaluates it.\n"
+        };
+
+        return response;
+    } catch (error) {
+        errorResponse(error.message, context.awsRequestId, callback);
     }
-    
-    if (default_tags && default_tags.length != 0) {
-        default_tags.forEach(function(tag) {
-            tags.push(tag);
-        });
-    }
 
-    var item = {
-        requestid: uuid.v1(),
-        userid: userid,
-        email: event.requestContext.authorizer.claims.email,
-        team: event.requestContext.authorizer.claims['custom:team'],
-        inline_policy: inline_policy,
-        policy: broker_policy,
-        tags: tags,
-        sessionDuration: sessionDuration,
-        timestamp: new Date().getTime()
-    };
-
-    var params = {
-        TableName: process.env.REQUESTS_TABLE,
-        Item: removeEmptyStringElements(item),
-    };
-
-    await docClient.put(params).promise();
-
-    const response = {
-        statusCode: 200,
-        body: "Permission request made by user '" + userid + "' (" + event.requestContext.authorizer.claims.email + ") . Please allow some time until the security admin evaluates it.\n"
-    };
-
-    return response;
 };
 
 function errorResponse(errorMessage, awsRequestId, callback) {
-    callback(null, {
+    const resp = {
         statusCode: 500,
-        body: JSON.stringify({
-            Error: errorMessage,
-            Reference: awsRequestId,
-        }),
+        body: errorMessage,
         headers: {
             'Access-Control-Allow-Origin': '*',
         },
-    });
+    };
+    callback(null, resp);
 }
